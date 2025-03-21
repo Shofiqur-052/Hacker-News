40ea3be3a74ca09bd87c83aaf5d8bd3d34f5de3f
shofiq <shofiq@appscode.com>
2025-03-21 16:07:41 +0600

    intregrated comment in store

--- src/store/index.js
+++ src/store/index.js
@@ -10,44 +10,24 @@ const store = createStore({
             comments: { },
         };
     },
-    mutations: {        
-
+    mutations: {
         setStory(state, { section, id, payload }) {
             state[section][id] = payload;
         },
-
-        setTopstoriesIDs(state, payload) {
-            state.topstoriesIDs = payload;
+        setIDs(state, { section, payload }) {
+            state[`${section}IDs`] = payload;
         },
-        setNewstoriesIDs(state, payload) {
-            state.newstoriesIDs = payload;
-        },
-        setBeststoriesIDs(state, payload) {
-            state.beststoriesIDs = payload;
-        },
-        setShowstoriesIDs(state, payload) {
-            state.showstoriesIDs = payload;
-        },
-        setAskstoriesIDs(state, payload) {
-            state.askstoriesIDs = payload;
-        },
-        setJobstoriesIDs(state, payload) {
-            state.jobstoriesIDs = payload;
-        },
-
         setComment(state, { id, payload }) {
             state.comments[id]= payload;
         },
-
         setUnixTime(state, { section, id }) {
             state[section][id].time = calculateTime(state[section][id].time);
         },
-
     },
     actions: {
         async fetchStories({commit}, section) {
             const res = await axios.get(`https://hacker-news.firebaseio.com/v0/${section}.json`);
-            commit(`set${section.charAt(0).toUpperCase() + section.slice(1)}IDs`, res.data);
+            commit('setIDs', { section: section, payload: res.data });
         },
         async fetchComment({commit}, id) {
             const res = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
